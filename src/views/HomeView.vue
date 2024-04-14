f50ea02a062772f5ec526467e75bd6fc53e13305
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-04-14 22:53:09 +0630

    Reactive value with reactive method. It can not be used in string, boolean, number like primitive value

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,33 @@
 <template>
   <div class="home">
-    <h1>Hello {{ name }}. I'm {{ age }} years old.</h1>
-    <input type="text" v-model="name">
-    <button @click="handleClick">Click</button>
-    <button @click="age++">increase age</button>
+    <h1>Person One</h1>
+    <p>{{ personOne.name }} - {{ personOne.age }}</p>
+    <button @click="changeOne">change one</button>
+
+    <h1>Person Two</h1>
+    <p>{{ personTwo.name }} - {{ personTwo.age }}</p>
+    <button @click="changeTwo">change two</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 export default {
   setup() {
-    let name = ref("KTZ"); // reactive value
-    let age = ref(27);
-
-    let handleClick = () => {
-      name.value = "PTR";
+    let personOne = ref({name: "Kyaw", age: 27});
+    let changeOne = () => {
+      personOne.value.name = "CKyaw";
     };
-    return {name, age, handleClick};
+
+    let personTwo = reactive({name: "Kyaw 2", age: 27});
+    let changeTwo = () => {
+      personTwo.name = "CKyaw 2";
+    };
+    // reactive not point to value but can not use in 'string, boolean, number, (primitive value)
+    // reactive("string");
+
+    return {personOne, changeOne, personTwo, changeTwo};
   }
 }
 </script>
