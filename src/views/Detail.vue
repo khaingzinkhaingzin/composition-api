9644312c0830faaffec4b80230fabdf0834e00de
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-04-15 16:53:09 +0630

    post detail page and getPost composable

---
+++ src/views/Detail.vue
@@ -0,0 +1,24 @@
+<template>
+	<div v-if="error">{{ error }}</div>
+	<div v-else>
+		<div v-if="post">
+			<h1>{{ post.title }}</h1>
+			<p>{{ post.body }}</p>
+		</div>
+		<div v-else>loading...</div>
+	</div>
+</template>
+
+<script>
+import getPost from "../composables/getPost";
+export default {
+	props: ["id"],
+	setup(props) {
+		let { post, error, load } = getPost(props.id);
+		load();
+		return { post, error };
+	},
+};
+</script>
+
+<style></style>
