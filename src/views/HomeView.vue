edd08f462281c0556bae0457f7c644fab0f21c8f
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-04-15 16:19:36 +0630

    getPosts composable function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,26 +12,13 @@
 
 <script>
 import PostsList from '../components/PostsList'
-import { ref } from 'vue';
+import getPosts from '../composables/getPosts'
 export default {
   components: { PostsList },
   setup() {
-    let posts = ref([]);
-    let error = ref("");
-
-    let load = async () => {
-      try {
-        let response = await fetch("http://localhost:3000/posts");
-        if (response.status === 404) {
-          throw new Error("not found url");
-        }
-        let datas = await response.json();
-        
-        posts.value = datas;
-      } catch(err) {
-        error.value = err.message;
-      }
-    };
+    // composable function 
+    let {posts, error, load} = getPosts();
+    
     load();
 
     return { posts, error };
