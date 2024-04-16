9671f343dc646d82a51f6ffd4d12fbe7c1bd903c
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-04-16 11:48:20 +0630

    Post detail page style

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,8 +1,8 @@
 <template>
 	<div v-if="error">{{ error }}</div>
 	<div v-else>
-		<div v-if="post">
-			<h1>{{ post.title }}</h1>
+		<div v-if="post" class="post">
+			<h2>{{ post.title }}</h2>
 			<p>{{ post.body }}</p>
 		</div>
 		<div v-else>
@@ -25,4 +25,39 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.post {
+    margin: 0 40px 30px;
+    padding-bottom: 30px;
+    border-bottom: 1px dashed #e7e7e7;
+}
+.post h2 {
+    display: inline-block;
+    position: relative;
+    font-size: 26px;
+    color: white;
+    margin-bottom: 10px;
+    max-width: 400px;
+}
+.post h2::before {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    background: #ff8800;
+    position: absolute;
+    z-index: -1;
+    padding-right: 40px;
+    left: -30px;
+    transform: rotateZ(-1deg);
+
+}
+.pill {
+    background-color: #f6f6f6;;
+    border: 1px solid #aaa;
+    border-radius: 5px;
+    display: inline-block;
+    padding: 1px 5px;
+    margin: 5px 3px;
+}
+</style>
